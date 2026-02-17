9d49eff46a5c74ad2d90554299440d85797971dc
Mishra <mishra65@exbp.roche.com>
2026-02-17 12:14:11 +0530

    Caffeine cache configuration

---
+++ src/main/java/com/example/springRedis/config/CaffeineConfig.java
@@ -0,0 +1,24 @@
+package com.example.springRedis.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CaffeineConfig {
+
+    @Bean
+    public CaffeineCacheManager cacheManager(){
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .expireAfterWrite(10, TimeUnit.MINUTES)
+                        .maximumSize(1000)
+        );
+        return cacheManager;
+    }
+
+}
